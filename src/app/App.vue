dea3bfcc8111e247fd70e6607fbe69630b1d6aef
Julia <bela@d-element.ru>
2024-10-26 17:07:25 +0500

    feat: add boilerplate

---
+++ src/app/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main class="container">
+    Main
+  </main>
+</template>
+
+<style lang="scss">
+</style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import './app/styles/main.css'
+
+import { createApp } from 'vue'
+import App from './app/App.vue'
+
+createApp(App).mount('#app')
